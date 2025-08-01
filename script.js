567e68799aaa8ec35761b511f10e95b6c42af211
Jazmin1952 <jazminb1952@gmail.com>
2025-08-01 17:26:46 +0000

    Added javascript components

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Hello World!");
+
+ //String has quotes/Number has no quotes//
+ let myTest = "123";
+ console.log(typeof myTest);
+
+ console.log(10 +" eggs");
+ console.log(10 + 8 + " eggs");
+ console.log("eggs " + 10 + 8);
+
+ //Both conditions need to be true //
+ console.log(7>3)&&(5>2);
+
+ //Only one condition must be true//
+ console.log(10>3)||(10>12);
+
+ //Logical not//
+ console.log(!5>3);
+
+ function introduction(name,age){
+     console.log("Hello, my name is " + name + " and I am "+ age + " years old.");
+ }
+
+ introduction("Jazmin", 16)
+
+const hour = new Date().getHours();
+let greeting ="";
+
+if (hour<12){
+    greeting = "Good morning 🌞";
+}else if (hour>18){
+    greeting = "Good afternoon🌙";
+}else{
+    greeting= "Good Evening🌤️";
+}
+
+document.getElementById("greeting").innerText = greeting;
