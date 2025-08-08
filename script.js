31598bd0e29792fd7c68ae5c740786fe403b8d95
Jazmin1952 <jazminb1952@gmail.com>
2025-08-08 17:26:57 +0000

    Added our testimonial page

--- script.js
+++ script.js
@@ -1,37 +1,142 @@
-console.log("Hello World!");
+const review = [
+    {
+        id: 1,
+        name: 'Susan Smith',
+        job: 'Web Developer',
+        img: '#',
+        text:'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+    },
+    {
+        id: 2,
+        name: 'Anna Johnson',
+        job: 'Wed Designer',
+        img: '#',
+        text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+    },
+    {
+        id: 3,
+        name: 'John Jones',
+        job: 'Teacher',
+        img: '#',
+        text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor.',
+    },
+];
 
- //String has quotes/Number has no quotes//
- let myTest = "123";
- console.log(typeof myTest);
+const img = document.getElementById('person-img');
+const author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
 
- console.log(10 +" eggs");
- console.log(10 + 8 + " eggs");
- console.log("eggs " + 10 + 8);
+const preBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn')
 
- //Both conditions need to be true //
- console.log(7>3)&&(5>2);
+let currentItem = 0;
 
- //Only one condition must be true//
- console.log(10>3)||(10>12);
+//Load initail item//
+window.addEventListener('DOMContentLoaded', function () {
+    const item = reviews[currentItem];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+});
 
- //Logical not//
- console.log(!5>3);
-
- function introduction(name,age){
-     console.log("Hello, my name is " + name + " and I am "+ age + " years old.");
- }
-
- introduction("Jazmin", 16)
-
-const hour = new Date().getHours();
-let greeting ="";
-
-if (hour<12){
-    greeting = "Good morning 🌞";
-}else if (hour>18){
-    greeting = "Good afternoon🌙";
-}else{
-    greeting= "Good Evening🌤️";
+function showPerson(person){
+    const item = reviews[person];
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
 }
 
-document.getElementById("greeting").innerText = greeting;
+nextBtn.addEventListener('click', function (){
+    currentItem++;
+    if(currentItem > reviews.length - 1){
+        currentItem = 0;
+    }
+    showPerson(currentItem);
+});
+
+preBtn.addEventListener('click', function (){
+    currentItem--;
+    if (currentItem < 0){
+        currentItem = reviews.length - 1;
+    }
+    showPerson(currentItem);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello World!");
+
+//  //String has quotes/Number has no quotes//
+//  let myTest = "123";
+//  console.log(typeof myTest);
+
+//  console.log(10 +" eggs");
+//  console.log(10 + 8 + " eggs");
+//  console.log("eggs " + 10 + 8);
+
+//  //Both conditions need to be true //
+//  console.log(7>3)&&(5>2);
+
+//  //Only one condition must be true//
+//  console.log(10>3)||(10>12);
+
+//  //Logical not//
+//  console.log(!5>3);
+
+//  function introduction(name,age){
+//      console.log("Hello, my name is " + name + " and I am "+ age + " years old.");
+//  }
+
+//  introduction("Jazmin", 16)
+
+// const hour = new Date().getHours();
+// let greeting ="";
+
+// if (hour<12){
+//     greeting = "Good morning 🌞";
+// }else if (hour>18){
+//     greeting = "Good afternoon🌙";
+// }else{
+//     greeting= "Good Evening🌤️";
+// }
+
+// document.getElementById("greeting").innerText = greeting;
